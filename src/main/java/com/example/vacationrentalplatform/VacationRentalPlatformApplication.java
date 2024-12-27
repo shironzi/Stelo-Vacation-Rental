b16c3674728115425715579a8359e5a34f32e0d6
shironzi <157195223+shironzi@users.noreply.github.com>
2024-12-28 00:43:39 +0800

    Add files via upload

---
+++ src/main/java/com/example/vacationrentalplatform/VacationRentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacationrentalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationRentalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationRentalPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vacationrentalplatform/VacationRentalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacationrentalplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationRentalPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
